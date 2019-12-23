746f6e7daa96d78000698d472f5d280af8a5d992
aarc97 <aarc97@gmail.com>
2019-12-23 09:14:07 -0400

    Commit inicial

---
+++ config/yargs.js
@@ -0,0 +1,24 @@
+//requires
+const opts = {
+  base: {
+    demand: true,
+    alias: "b"
+  },
+  limite: {
+    alias: "l",
+    default: 10
+  }
+};
+
+const argv = require("yargs")
+  .command("listar", "Imprime en consola la tabla de multiplicar", opts)
+  .command(
+    "crear",
+    "Genera en consola la tabla de multiplicar con un limite",
+    opts
+  )
+  .help().argv;
+
+module.exports = {
+  argv
+};
